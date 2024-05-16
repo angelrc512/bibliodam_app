81af26cb47722ccdb45a4bed474cba76d35f599f
“Angel“ <angel.todoinformatica@gmail.com>
2024-05-16 12:38:53 +0200

    2º parte. Firebase conectado

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() async {
+Future <void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
    await Firebase.initializeApp();
     runApp(const BiblioApp());
